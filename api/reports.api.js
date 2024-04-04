d5516dc9d8c85ff800e57e6656756af0a8cfbb8a
Adrian <marcelonads@gmail.com>
2024-04-04 08:41:07 +0800

    setup: SETUP of api, service, repository files

---
+++ api/reports.api.js
@@ -0,0 +1,84 @@
+const TokenMiddleware = require("../middlewares/TokenMiddleware"); // Remove this if unused
+const {
+	ROLES,
+	RoleManagementMiddleware,
+} = require("../middlewares/RoleManagementMiddleware");
+
+const { validationResult, body } = require("express-validator");
+
+const logger = require("../config/winston");
+
+// Import your SERVICE HERE
+// Import MISC HERE
+
+/**
+ * @param {import('express').Express} app
+ */
+module.exports = (app) => {
+	const tokenMiddleware = new TokenMiddleware();
+	const roleMiddleware = new RoleManagementMiddleware();
+
+	/**
+	 * This function will be used by the express-validator for input validation,
+	 * and to be attached to APIs middleware.
+	 * @param {*} req
+	 * @param {*} res
+	 */
+	function validate(req, res) {
+		const ERRORS = validationResult(req);
+
+		if (!ERRORS.isEmpty()) {
+			throw new HttpUnprocessableEntity(
+				"Unprocessable Entity",
+				ERRORS.mapped()
+			);
+		}
+	}
+
+	app.get(
+		"/admin_reports/api/v1/dashboard",
+		[
+			tokenMiddleware.AccessTokenVerifier(),
+			roleMiddleware.CheckRole(ROLES.ADMIN),
+		],
+
+		/**
+		 * @param {import('express').Request} req
+		 * @param {import('express').Response} res
+		 */
+		async (req, res) => {
+			try {
+				logger.info({
+					DASHBOARD_REQUEST: {
+						role: req.role,
+						message: "SUCCESS",
+					},
+				});
+
+				logger.info({
+					DASHBOARD_RESPONSE: {
+						role: req.role,
+						message: "SUCCESS",
+					},
+				});
+
+				return res
+					.status(200)
+					.json({ status: 200, data: [], message: "Success" });
+			} catch (err) {
+				logger.error({
+					DASHBOARD_ERROR: {
+						err,
+						message: err.message,
+					},
+				});
+
+				return res.status(err.status || 500).json({
+					status: err.status || 500,
+					data: err.data || [],
+					message: err.message || "Internal Server Error",
+				});
+			}
+		}
+	);
+};
